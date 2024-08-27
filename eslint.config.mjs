a73177ca93e8905b70eaa53974d8eb913d6ec137
Ashutosh Verma <49877083+ashutosh1556@users.noreply.github.com>
2024-08-27 09:56:21 +0530

    Added new rules to eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -38,4 +38,25 @@ export default [
 
   // Include the recommended rules from the eslint-plugin-react plugin (flat configuration)
   pluginReact.configs.flat.recommended,
+
+  {
+    rules: {
+      "cypress/no-assigning-return-values": "error",
+      "cypress/no-unnecessary-waiting": "error",
+      "cypress/assertion-before-screenshot": "warn",
+      "cypress/no-force": "warn",
+      "cypress/no-async-tests": "error",
+      "cypress/no-async-before": "error",
+      "cypress/no-pause": "error",
+      "cypress/no-debug": "error",
+    },
+  },
+  {
+    env: {
+      "cypress/globals": true,
+    },
+  },
+  {
+    extends: ["plugin:cypress/recommended"],
+  },
 ];
